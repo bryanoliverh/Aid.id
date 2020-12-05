5e06aa91809e0357c1e5d42e92adba8d4fba446f
philipusrenaldy <philipusrenaldy@users.noreply.github.com>
2020-12-05 16:48:16 +0700

    update

--- app/src/main/java/com/aidid/firebase/HomeFragment.kt
+++ app/src/main/java/com/aidid/firebase/HomeFragment.kt
@@ -1,10 +1,20 @@
 package com.aidid.firebase
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_get_started.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.menulayout.view.*
+import kotlin.collections.ArrayList
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,25 +26,78 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
+    //var adapter : ItemAdapter? = null
+    //var itemlist =  ArrayList<Item>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /*itemlist.add(Item("Hospital", "For the Healty world !", R.drawable.hospital))
+        itemlist.add(Item("Orphanage", "For the World the better one !", R.drawable.agreement))
+        itemlist.add(Item("Antivirus", "For the New Normal !", R.drawable.antivirus))
+
+        adapter = ItemAdapter(this, itemlist)*/
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
 
+    /*class ItemAdapter : BaseAdapter{
+
+        var itemlist = ArrayList<Item>()
+        var context:Context? = null
+
+        constructor(itemlist: ArrayList<Item>, context: Context?) : super() {
+            this.itemlist = itemlist
+            this.context = context
+        }
+
+        override fun getCount(): Int {
+            return itemlist.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return position
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        @SuppressLint("ViewHolder", "InflateParams")
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val item = this.itemlist[position]
+            val inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val itemview = inflater.inflate(R.layout.menulayout,null)
+            itemview.imageView10.setImageResource(item.image!!)
+            itemview.textView10.text = item.name!!
+            return itemview
+        }
+
+    }*/
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+
+        view.hospital.setOnClickListener { view ->
+            inflater.inflate(R.layout.activity_hospital,container, false)
+        }
+
+        view.orphanage.setOnClickListener { view ->
+            Log.d("Orphanage", "Selected")
+        }
+
+        return view
     }
 
     companion object {
---
+++ app/src/main/java/com/aidid/firebase/Item.kt
@@ -0,0 +1,13 @@
+package com.aidid.firebase
+
+/*class Item {
+    var name : String? = null
+    var des : String? = null
+    var image : Int? = null
+
+    constructor(name: String?, des: String?, image: Int?) {
+        this.name = name
+        this.des = des
+        this.image = image
+    }
+}*/
