b971eefdcf4d32e7a995249befc1a7557367c0b6
philipusrenaldy <philipusrenaldy@users.noreply.github.com>
2020-12-13 18:23:04 +0700

    update

--- app/src/main/java/com/aidid/firebase/HomeActivity.kt
+++ app/src/main/java/com/aidid/firebase/HomeActivity.kt
@@ -12,15 +12,20 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(){
     private lateinit var auth: FirebaseAuth
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
         auth = FirebaseAuth.getInstance()
+
+        val fragment1 = HomeFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment, fragment1)
+        transaction.commit()
+
         val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration.Builder(
             R.id.nav_home, R.id.nav_Profile
@@ -29,19 +34,14 @@ class HomeActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         nav_bottom.setupWithNavController(navController)
 
-        val fragment1 = HomeFragment()
-        supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment1)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean
-    {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean{
         menuInflater.inflate(R.menu.option_menu, menu)
             return true
     }
-
         override fun onOptionsItemSelected(item: MenuItem): Boolean
         {
-
             when(item.itemId)
            {
                R.id.logout->{
@@ -52,18 +52,18 @@ class HomeActivity : AppCompatActivity() {
                }
             return true
            }
-                R.id.settings-> {
+                R.id.settings->
+                {
                     Intent(this@HomeActivity, SettingsActivity::class.java).also {
                         it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                         startActivity(it)
                     }
                     return true
                 }
-        else -> return true
+            else -> return true
            }
 
         }
-
-            }
+}
 
 
--- app/src/main/java/com/aidid/firebase/HomeFragment.kt
+++ app/src/main/java/com/aidid/firebase/HomeFragment.kt
@@ -10,43 +10,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.ImageButton
 import kotlinx.android.synthetic.main.activity_get_started.*
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 import kotlinx.android.synthetic.main.menulayout.view.*
 import kotlin.collections.ArrayList
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class HomeFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle? ): View? {
 
-        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
-        return view
+        val v: View = inflater.inflate(R.layout.fragment_home, container, false)
+        val btn1 = v.findViewById<View>(R.id.buttonhospital) as ImageButton
+
+        btn1.setOnClickListener {
+            val fragment = HospitalFragment()
+            val fragmentManager =  requireActivity().supportFragmentManager
+            val transaction = fragmentManager.beginTransaction()
+            transaction.replace(R.id.nav_host_fragment, fragment)
+            transaction.commit()
+        }
+        return v
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().title  = "Fragment Home"
     }
 }
