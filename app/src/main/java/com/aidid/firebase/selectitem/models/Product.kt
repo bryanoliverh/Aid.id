e7b72980644701409cef33d61b427c93840ec109
bryanoliverh <bryanoliverh@gmail.com>
2020-12-17 22:15:58 +0700

    add to cart donation, confirmation, back icon for settings

---
+++ app/src/main/java/com/aidid/firebase/selectitem/models/Product.kt
@@ -0,0 +1,13 @@
+package com.aidid.firebase.selectitem.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Product (
+    var id : Int? = null,
+    var name : String? = null,
+    var points : Int? = null,
+    var image : String? = null,
+    var selectedQuantity : Int? = null
+) : Parcelable
